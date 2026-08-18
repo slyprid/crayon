d21ec2946bfbf854bb2e493e9692a8e7987044e6
Aaron Foley <afoley@jointcommission.org>
2026-08-18 14:37:39 -0400

    Add basic application window with winit
    Integrates the `winit` crate to set up a fundamental application window
    and event loop. This change establishes the initial groundwork for a
    graphical user interface, temporarily replacing the previous CLI
    entry point.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 use clap::Parser;
+use winit:: {
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::{Window, WindowAttributes, WindowId}
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -7,8 +18,25 @@ struct Args {
     file: String
 }
 
-fn main() {
+fn main() -> Result<(), winit::error::EventLoopError> {
     let args = Args::parse();
+    let event_loop = EventLoop::new()?;
+    let mut app = App::default();
+    event_loop.run_app(&mut app)
 
-    println!("Parsing file: {}", args.file);
+    //println!("Parsing file: {}", args.file)
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let attrs = WindowAttributes::default().with_title("Crayon");
+        let window = event_loop.create_window(attrs).expect("create window");
+        self.window = Some(window);
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
+        if let WindowEvent::CloseRequested = event {
+            event_loop.exit();
+        }
+    }
 }
