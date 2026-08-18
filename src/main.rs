a1dc5e817c5b0c72c17e1ccb9bf86275ffd50d47
Aaron Foley <afoley@jointcommission.org>
2026-08-18 17:17:09 -0400

    Add pixel buffer and basic drawing capability
    Integrates the `pixels` crate to provide a hardware-accelerated pixel buffer. This establishes the initial rendering surface, allowing the application to draw directly to the window and displaying a test color. A new `colors` module is introduced to manage a palette.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+///////////////////////////////////////
+/// MODS
+///////////////////////////////////////
+mod colors;
+
+///////////////////////////////////////
+/// USINGS
+///////////////////////////////////////
 use clap::Parser;
+use pixels::{Pixels, SurfaceTexture};
 use winit:: {
     application::ApplicationHandler,
     event::WindowEvent,
     event_loop::{ActiveEventLoop, EventLoop},
     window::{Window, WindowAttributes, WindowId}
 };
+use winit::dpi::LogicalSize;
+use colors::{Colors, get_color};
 
+///////////////////////////////////////
+/// CONSTANTS
+///////////////////////////////////////
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
+
+
+///////////////////////////////////////
+/// STRUCTS
+///////////////////////////////////////
 #[derive(Default)]
 struct App {
     window: Option<Window>,
@@ -18,6 +39,9 @@ struct Args {
     file: String
 }
 
+///////////////////////////////////////
+/// MAIN
+///////////////////////////////////////
 fn main() -> Result<(), winit::error::EventLoopError> {
     let args = Args::parse();
     let event_loop = EventLoop::new()?;
@@ -27,16 +51,63 @@ fn main() -> Result<(), winit::error::EventLoopError> {
     //println!("Parsing file: {}", args.file)
 }
 
+///////////////////////////////////////
+/// IMPLEMENTATIONS
+///////////////////////////////////////
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let attrs = WindowAttributes::default().with_title("Crayon");
+        let attrs = WindowAttributes::default()
+            .with_title("Crayon")
+            .with_inner_size(LogicalSize::new((WIDTH * 2) as f64, (HEIGHT * 2) as f64));
+
         let window = event_loop.create_window(attrs).expect("create window");
         self.window = Some(window);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
-        if let WindowEvent::CloseRequested = event {
-            event_loop.exit();
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::RedrawRequested => {
+                if let Some(window) = self.window.as_ref() {
+                    let size = window.inner_size();
+                    let surface_texture = SurfaceTexture::new(size.width, size.height, window);
+                    let mut pixels = Pixels::new(WIDTH, HEIGHT, surface_texture).expect("create pixel buffer");
+
+                    let frame = pixels.frame_mut();
+                    for (i, px) in frame.chunks_exact_mut(4).enumerate() {
+                        let x = (i as u32) % WIDTH;
+                        let y = (i as u32) / WIDTH;
+
+                        // let r = (x * 255 / WIDTH) as u8;
+                        // let g = (y * 255 / HEIGHT) as u8;
+                        // let b = 128u8;
+                        // let a = 255u8;
+                        let c = get_color(Colors::BrightGreen);
+                        let r = c.0 as u8;
+                        let g = c.1 as u8;
+                        let b = c.2 as u8;
+                        let a = c.3 as u8;
+
+                        px.copy_from_slice(&[r, g, b, a]);
+                    }
+
+                    if pixels.render().is_err() {
+                        event_loop.exit();
+                    }
+                }
+            },
+            WindowEvent::Resized(size) => {
+                if let Some(window) = self.window.as_ref() {
+                    window.request_redraw();
+                }
+            }
+            _ => { }
+        }
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        if let Some(window) = self.window.as_ref() {
+            window.request_redraw();
         }
     }
 }
